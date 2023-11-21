f43e27864cac3ec25857fecec239a9d666346721
Athul Mohan <mohanathul71@gmail.com>
2023-11-21 16:46:51 +0530

    initial

---
+++ src/main/java/com/spring/mongo/demo/config/DatabaseConfiguration.java
@@ -0,0 +1,52 @@
+package com.spring.mongo.demo.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class DatabaseConfiguration extends AbstractMongoClientConfiguration {
+    Logger logger = LoggerFactory.getLogger(DatabaseConfiguration.class);
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String dataBase;
+
+    public DatabaseConfiguration() {
+    }
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return this.dataBase;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        String connectionStr = uri;
+        ConnectionString connectionString = new ConnectionString(connectionStr);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+}
+
